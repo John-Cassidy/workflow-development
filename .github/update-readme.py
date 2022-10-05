deabf41a122e78c0cfb5665c27c9c810aa939b91
John Cassidy <jpcassidy@outlook.com>
2022-10-05 13:50:43 -0400

    create python file

---
+++ .github/update-readme.py
@@ -0,0 +1,13 @@
+repo_name = "workflow-development"
+owner = "John-Cassidy"
+owner_repo_name = f"{owner}/{repo_name}"
+
+statusFiles = []
+
+statusFiles.append("history/file1.csv")
+statusFiles.append("history/file2.csv")
+statusFiles.append("history/file3.csv")
+statusFiles.append("history/file4.csv")
+statusFiles.append("history/file5.csv")
+
+print(f"::set-output name=statusFiles::{statusFiles}")
